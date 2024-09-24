eb4a0b77a190a163b6056b74e76dcadf05513587
nguyencongmen <nguyenme123321@gmail.com>
2024-09-24 08:09:40 +0700

    update oop

---
+++ Oop/MyInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+interface IA {
+  public function NotDeclare();
+
+}
+
+interface IB{
+   
+
+}
+interface IC{
+   
+
+}
+
+ interface Interface1 {
+
+}
+
+
+ interface Interface2 {
+
+ }
+
+ interface Interface3 {
+
+ }
